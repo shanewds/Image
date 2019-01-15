95e99f5709583fe5400494d8425400c3d1cd299a
jack_global <1987780732@qq.com>
2019-01-15 11:11:11 +0800

    ok

---
+++ src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String Nasid="AC1";
+		System.out.println(Nasid.length());
+		
+		String Nasid2="AC1  ".trim();
+		System.out.println(Nasid2.length());
+	}
+	
+}
